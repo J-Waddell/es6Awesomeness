2b0d749947f53a5cedde8d3cbe73603c9a406209
Jesse Waddell <jesse@Jesses-MacBook-Pro.local>
2017-03-01 13:49:45 -0600

    cheer complete found typo

---
+++ cheer.js
@@ -0,0 +1,4 @@
+var array = ['Give me a J!', 'Give me an O!', 'Give me an H!', 'Give me an N!', 'Give me a D!', 'Give me an O', 'Give me an E!', 'What does that spell?', 'JOHN DOE!']
+for (let elem of array) {
+    console.log(elem)
+}
