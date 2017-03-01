9007207798306e31b96a627bfa623e04c5e4ab36
Jesse Waddell <jesse@Jesses-MacBook-Pro.local>
2017-03-01 14:31:28 -0600

    cleaned up the strings

--- cheer.js
+++ cheer.js
@@ -1,9 +1,26 @@
-var array = ['Give me a J!', 'Give me an O!', 'Give me an H!', 'Give me an N!', 'Give me a D!', 'Give me an O!', 'Give me an E!', 'What does that spell?', 'JOHN DOE!']
+var array = ['Give me a J!', 
+    'Give me an O!', 
+    'Give me an H!', 
+    'Give me an N!', 
+    'Give me a D!', 
+    'Give me an O!', 
+    'Give me an E!', 
+    'What does that spell?', 
+    'JOHN DOE!']
+
 for (let elem of array) {
     console.log(elem)
 }
 
-let letters = ['Give me a J!', 'Give me an O!', 'Give me an H!', 'Give me an N!', 'Give me a D!', 'Give me an O!', 'Give me an E!', 'What does that spell?', 'JOHN DOE!']
+let letters = ['Give me a J!', 
+    'Give me an O!', 
+    'Give me an H!', 
+    'Give me an N!', 
+    'Give me a D!', 
+    'Give me an O!',
+    'Give me an E!', 
+    'What does that spell?', 
+    'JOHN DOE!']
 
 let [J, O, H, N, D, O2, E,...theRest] = letters
 console.log(letters)
